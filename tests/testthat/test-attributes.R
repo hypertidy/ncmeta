7009fa83c3e773ecf5f1714bbfde763a8748739e
Michael Sumner <mdsumner@gmail.com>
2017-06-25 17:49:21 +1000

    more tests

--- tests/testthat/test-attributes.R
+++ tests/testthat/test-attributes.R
@@ -2,7 +2,37 @@ context("attributes")
 f <- system.file("extdata", "S2008001.L3m_DAY_CHL_chlor_a_9km.nc", package = "ncmeta")
 u <- "https://upwell.pfeg.noaa.gov/erddap/tabledap/FRDCPSTrawlLHHaulCatch"
 
-test_that("multiplication works", {
-  nc_atts(f)
-nc_atts(u)
+test_that("attributes works", {
+  da <- nc_atts(f) %>% expect_s3_class("tbl_df") %>% 
+    expect_named(c("attribute", "variable", "value")) 
+  expect_that(nrow(da), equals(87L))
+  expect_that(da$value, is_a("list"))
+  du <- nc_atts(u) %>%  expect_s3_class("tbl_df") %>% 
+    expect_named(c("attribute", "variable", "value"))
+  expect_that(nrow(du), equals(119L))
+  expect_that(du$value, is_a("list"))
+  
+  })
+
+test_that("individual attribute inquiry works", {
+  nc_att(f, 0, 0) %>% expect_s3_class("tbl_df") %>% 
+    expect_named(c("attribute", "variable", "value")) 
+  a3 <- nc_att(f, 0, 3)  
+  expect_that(a3$attribute, equals(3.0))
+  expect_that(a3$value, equals(list(-32767)))
+})
+
+l3binfile <- system.file("extdata", "S2008001.L3b_DAY_CHL.nc", package = "ncmeta")
+
+nc_atts(l3binfile)
+test_that("failure is graceful", {
+  expect_warning(nc_atts(l3binfile), "no variables recognizable")
+  abin <- nc_atts(l3binfile)
+  abin %>%  expect_s3_class("tbl_df") %>% 
+    expect_named(c("id", "name", "type", "ndims", "dimids", "natts")) 
+  expect_that(nrow(abin), equals(1L))
+  expect_that(abin$name, equals("NC_GLOBAL"))
+  expect_that(abin$id, equals(-1))
+  expect_true(all(is.na(c(abin$dimids, abin$ndims))))
+  expect_that(abin$natts, equals(49))
 })
