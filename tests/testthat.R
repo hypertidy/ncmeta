6666410b50feb737c408a22ffae9d259385a48f8
Michael Sumner <mdsumner@gmail.com>
2020-05-12 21:26:19 +1000

    docs

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,7 @@
-library(testthat)
-library(ncmeta)
-skip_on_os("solaris")
-test_check("ncmeta")
+sysname <- tolower(Sys.info()[["sysname"]])
+if (!"sunos" %in% sysname) {
+  library(testthat)
+  library(ncmeta)
+  #skip_on_os("solaris")
+  test_check("ncmeta")
+}
